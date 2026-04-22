c4b315d1967e9e3021d36dab73ef1fd2ec11985b
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-04-22 18:07:20 +0530

    Added solution of Climbing Stairs

---
+++ Dynamic_Programming/LC_70_ClimbingStairs.java
@@ -0,0 +1,17 @@
+package Dynamic_Programming;
+import java.util.Arrays;
+class LC_70_ClimbingStairs {
+    public int climbStairs(int n) {
+        int []dp=new int[n+1];
+         Arrays.fill(dp, -1);
+        return solve(n, dp);
+       
+    }
+    int solve(int n,int dp[])
+    {
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n - 1, dp) + solve(n - 2, dp);
+
+    }
+}
