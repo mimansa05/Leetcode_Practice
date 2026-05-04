95152d2893630eafb0dc1bf2074d6f0545f51828
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-05-04 17:54:10 +0530

    Added solution of POTD

---
+++ DQ/LC_48.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int newMa[][] = new int[n][m];
+
+        int i = 0;  
+        int j = 0;   
+        while (i < n) {
+            while (j < m) {
+                newMa[j][n - 1 - i] = matrix[i][j];
+                j++;
+            }
+            i++;
+            j = 0;
+        }
+
+        for (int a = 0; a < n; a++) {
+            for (int b = 0; b < m; b++) {
+                matrix[a][b] = newMa[a][b];
+            }
+        }
+    }
+}
---
+++ Dynamic_Programming/LC_338_CountingBits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        for(int i=0;i<=n;i++)
+        {
+            dp[i]=CountBits(i,dp);
+        }
+        return dp;
+    }
+    int CountBits(int n,int dp[])
+    {
+        if(n==0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=(n%2)+CountBits(n/2,dp);
+        return dp[n];
+    }
+}
