c72eb066c01863cf3d9dcfba77e35110f823fbb9
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-07-03 10:40:43 +0530

    Added solution

---
+++ Math/LC_1822_SignofProductofArray.java
@@ -0,0 +1,11 @@
+class LC_1822_SignofProductofArray {
+    public int arraySign(int[] nums) {
+        int count=0;
+        for(int x:nums)
+        {
+            if(x==0) return 0;
+            if(x<0) count++;
+        }
+        return count%2==0?1:-1;
+    }
+}
