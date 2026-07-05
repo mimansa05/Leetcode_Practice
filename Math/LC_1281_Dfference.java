e531be48c9a87b4abb720339f86347452919116f
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-07-05 17:29:46 +0530

    Added solution

---
+++ Math/LC_1281_Dfference.java
@@ -0,0 +1,14 @@
+class LC_1281_Dfference {
+    public int subtractProductAndSum(int n) {
+        int p=1;
+        int sum=0;
+        while(n>0)
+        {
+            int d=n%10;
+            p*=d;
+            sum+=d;
+            n=n/10;
+        }
+        return (p-sum);
+    }
+}
