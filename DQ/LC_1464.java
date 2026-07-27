594b3d69843f26cb4336e13ab196bf0ab20fb4da
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-07-27 12:20:31 +0530

    Added solution of POTD

---
+++ DQ/LC_1464.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class LC_1464 {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        return (nums[n-1]-1)*(nums[n-2]-1);
+    }
+}
