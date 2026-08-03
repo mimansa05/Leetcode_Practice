b8789d32adbaebdb21b800355c948a002f0d44ea
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-08-03 10:56:38 +0530

    Added soltuion of Inorder Traversal

---
+++ Binary Tree/LC_94_InorderTraversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class LC_94_InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result=new ArrayList<>();
+        inorder(root,result);
+        return result;
+    }
+    void inorder(TreeNode root,List<Integer> result)
+    {
+        if(root==null) return;
+        inorder(root.left,result);
+        result.add(root.val);
+        inorder(root.right,result);
+    }
+}
