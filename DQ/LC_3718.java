b27394f72a077599ef9b99112eabcc46f2d3e1ff
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-08-25 13:50:00 +0530

    POTD

---
+++ DQ/LC_3718.java
@@ -0,0 +1,15 @@
+class LC_3718 {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> seen=new HashSet<>();
+        for(int num:nums)
+        {
+            seen.add(num);
+        }
+        int cur=k;
+        while(seen.contains(cur))
+        {
+            cur+=k;
+        }
+        return cur;
+    }
+}
