f45c173d1117c9ac859e98f23cf749604297ef7e
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-09-15 08:49:09 +0530

    Added solution of word ladder using bfs

---
+++ Graphs/LC_127_WordLadder.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class WordLadderBFS {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int n=wordList.size();
+        Set<String> set=new HashSet<>(wordList);
+        Queue<Pair<String,Integer>> q=new LinkedList<>();
+        q.add(new Pair<>(beginWord,1));
+        set.remove(beginWord);
+        while(!q.isEmpty())
+        {
+            String word=q.peek().getKey();
+            int steps=q.peek().getValue();
+            q.poll();
+
+            if(word.equals(endWord)) return steps;
+            for(int i=0;i<word.length();i++)
+            {
+                char arr[]=word.toCharArray();
+                char original=arr[i];
+                for(char ch='a';ch<='z';ch++)
+                {
+                    arr[i]=ch;
+                    String newWord=new String(arr);
+                    if(set.contains(newWord))
+                    {
+                        set.remove(newWord);
+                        q.add(new Pair<>(newWord,steps+1));
+                    }
+                }
+                arr[i]=original;
+            }
+        }
+        return 0;
+    }
+}
+class Pair<K,V>
+{
+    private K key;
+    private V value;
+    public Pair(K key, V value)
+    {
+        this.key=key;
+        this.value=value;
+    }
+    public K getKey() {return key;}
+    public V getValue() {return value;}
+}
