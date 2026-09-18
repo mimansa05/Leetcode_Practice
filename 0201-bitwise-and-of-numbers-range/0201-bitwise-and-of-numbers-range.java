87d1ede2af0fbb3a521504df0b27f0da48658fd6
Mimansa Sharma <mimansasharma308@gmail.com>
2026-09-18 16:56:42 +0530

    Time: 3 ms (100%), Space: 45.7 MB (76.45%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,21 +1,9 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        //Only those bits will be left that are common in the range
-/**RANGE AND
-   ↓
-COMMON PREFIX
-   ↓
-RIGHT SHIFT BOTH
-   ↓
-RESTORE LEFT SHIFT*/
-
-      int shift=0;
-      while(left!=right)
-      {
-        left >>=1;//for removing bits until bits same
-        right >>=1;
-        shift++;
-      }
-      return left << shift;// add number of bits that has been lost
+        while(right>left)
+        {
+            right=right & (right-1);
+        }
+        return right;
     }
 }
